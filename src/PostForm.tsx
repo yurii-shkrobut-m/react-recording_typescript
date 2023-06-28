5cb847bf976b4c225015e8a784cc947da9d55828
Misha Hrynko <misha@mate.academy>
2023-06-28 21:51:11 +0300

    before Create a Post

--- src/PostForm.tsx
+++ src/PostForm.tsx
@@ -3,15 +3,28 @@ import React, { useState } from 'react';
 
 export const PostForm: React.FC = () => {
   const [title, setTitle] = useState('');
-  const [hasTitleError, setHasTitleError] = useState(false);
+  const [hasTitleError, setHasTitleError] = useState(true);
 
   const [userId, setUserId] = useState(0);
+  const [hasUserIdError, setHasUserIdError] = useState(true);
+  
   const [body, setBody] = useState('');
+  const [hasBodyError, setHasBodyError] = useState(true);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
     setHasTitleError(false);
-  }
+  };
+
+  const handleUserIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleBodyChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -30,7 +43,9 @@ export const PostForm: React.FC = () => {
       onSubmit={handleSubmit}
     >
       <div className="field">
-        <label className="label" htmlFor="post-title">Title</label>
+        <label className="label" htmlFor="post-title">
+          Title
+        </label>
 
         <div className={classNames('control', {
           'has-icons-right': hasTitleError,
@@ -54,30 +69,57 @@ export const PostForm: React.FC = () => {
         </div>
 
         {hasTitleError && (
-          <p className="help is-danger">Title is required</p>
+          <p className="help is-danger">Please enter a title</p>
         )}
       </div>
 
       <div className="field">
-        <label className="label">Subject</label>
+        <label className="label" htmlFor="post-user-id">
+          Subject
+        </label>
+
         <div className="control has-icons-left">
-          <div className="select">
-            <select>
-              <option>Select dropdown</option>
-              <option>With options</option>
+          <div className={classNames('select', {
+            'is-danger': hasUserIdError,
+          })}>
+            <select
+              id="post-user-id"
+              value={userId}
+              onChange={handleUserIdChange}
+            >
+              <option value="0" disabled>Select a user</option>
             </select>
           </div>
+
           <span className="icon is-small is-left">
             <i className="fas fa-user"></i>
           </span>
         </div>
+
+        {hasUserIdError && (
+          <p className="help is-danger">Please select a user</p>
+        )}
       </div>
 
       <div className="field">
-        <label className="label">Message</label>
+        <label className="label">
+          Message
+        </label>
+
         <div className="control">
-          <textarea className="textarea" placeholder="Textarea"></textarea>
+          <textarea 
+            className={classNames('textarea', {
+              'is-danger': hasBodyError,
+            })} 
+            placeholder="Add some text here"
+            value={body}
+            onChange={handleBodyChange}
+          ></textarea>
         </div>
+
+        {hasBodyError && (
+          <p className="help is-danger">Please select a user</p>
+        )}
       </div>
 
       <div className="buttons">
