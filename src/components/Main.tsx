44ea58ba3ea30d7f6765272b8273c2a443efdf94
Yurii Shkrobut <georgebest2409@gmail.com>
2023-10-10 10:49:09 +0300

    Before Redux like State Managment with Context

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,63 @@
-import { HomePage } from './HomePage';
+import { useReducer } from "react";
+import { Lang } from "../types/Lang";
 
-export const Main = () => (
-  <main>
-    <HomePage />
-  </main>
-)
+type Action = { type: 'decrease'}
+            | { type: 'increase' }
+            | { type: 'add', payload: number }
+            | { type: 'setLang', payload: Lang };
+
+interface State {
+  amount: number;
+  lang: Lang;
+}
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'decrease':
+      return {
+        ...state,
+        amount: state.amount - 1,
+      };
+
+    case 'increase':
+      return {
+        ...state,
+        amount: state.amount + 1,
+      };
+
+    case 'add':
+      return {
+        ...state,
+        amount: state.amount + (action.payload || 0),
+      };
+
+    case 'setLang':
+      return {
+        ...state,
+        lang: action.payload,
+      };
+  
+    default:
+      return state;
+  }
+}
+
+const initialState: State = {
+  amount: 0,
+  lang: Lang.EN,
+}
+
+export const Main = () => {
+  const [{amount }, dispatch] = useReducer(reducer, initialState);
+
+  const decrease = () => dispatch({type: 'decrease'});
+  const increase = () => dispatch({type: 'increase'});
+
+  return (
+    <main>
+      <button onClick={decrease}>-</button>
+      {amount}
+      <button onClick={increase}>+</button>
+    </main>
+  )
+}
